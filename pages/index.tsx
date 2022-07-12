641057744b7cbd566f3e63d218743d8cf7eec95f
kadowning1 <kadowning1@gmail.com>
2022-07-12 10:39:20 -0400

    add Header and CTA

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import CTA from '../components/CTA'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <CTA />
     </div>
   )
 }
